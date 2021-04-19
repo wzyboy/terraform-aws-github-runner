f632dd3c46404b61d1c0686c8b41d6bc83d4147f
Richard Simpson <rsimpson@uship.com>
2021-04-18 20:22:27 -0500

    try and make run better in weird environment

--- modules/runners/templates/user-data.ps1
+++ modules/runners/templates/user-data.ps1
@@ -6,7 +6,7 @@ Start-Transcript -Path "C:\UserData.log" -Append
 ${pre_install}
 
 # Install Chocolatey
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 $env:chocolateyUseWindowsCompression = 'true'
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
@@ -16,6 +16,10 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Remove-Item Alias:curl
+Remove-Item Alias:wget
+
 refreshenv
 '@
 # Write it to the $profile location
@@ -26,8 +30,7 @@ Set-Content -Path "$PsHome\Microsoft.PowerShell_profile.ps1" -Value $ChocoProfil
 Write-Host "Installing curl..."
 choco install curl -y
 
-Remove-Item Alias:curl
-Remove-Item Alias:wget
+refreshenv
 
 Get-Command curl
 
@@ -41,18 +44,28 @@ while (!(Test-Path 'C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwa
     $loop++
     Start-Sleep -Seconds 5
 }
+
+$initScript = @'
 & 'C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1' -a fetch-config -m ec2 -s -c ssm:${ssm_key_cloudwatch_agent_config}
+'@
+Set-Content -Path "C:\SetupCloudwatch.ps1" -Value $initScript
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\SetupCloudwatch.ps1"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+Register-ScheduledTask -TaskName 'ReconfigureCloudwatchAgent' -TaskPath '\GitHubActionsRunner\' -Action $action -Trigger $trigger -Principal $principal -Description 'Reconfigures the Cloudwatch Agent' -ErrorAction Stop 
 %{ endif ~}
 
 # Install docker
 Write-Host "Initializing docker module..."
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$False
+Install-PackageProvider -Name NuGet -Force -Confirm:$False
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force -Confirm:$False
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Confirm:$False
 
 # Install dependent tools
 Write-Host "Installing additional development tools"
 choco install git jq awscli archiver -y
+refreshenv
 
 ${install_config_runner}
 ${post_install}
